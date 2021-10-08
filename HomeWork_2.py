d0b057001fce3608ddf11c6184059e3ba7931d0f
RuslanBog <90967296+RuslanBog@users.noreply.github.com>
2021-10-08 20:54:21 +0300

    Add files via upload

---
+++ HomeWork_2.py
@@ -0,0 +1,48 @@
+# value = input()
+# new_value = int(value)
+# if int(value) < 100:
+#     new_value = (int(value) / 2)
+#     print(new_value)
+# else:
+#     print(-new_value)
+
+#####################################
+
+# value = input()
+# new_value = int(value)
+# if int(value) < 100:
+#     new_value = 1
+# else:
+#     new_value = 0
+# print(new_value)
+
+#####################################
+
+# value = input()
+# new_value = int(value)
+# if int(value) < 100:
+#     new_value = True
+# else:
+#     new_value = False
+# print(new_value)
+
+######################################
+
+# my_str = "hillel"
+# print(my_str.upper())
+
+#####################################
+
+# my_str = "HILLEL"
+# print(my_str.lower())
+
+####################################
+
+# это не решил
+
+# my_str = "Hillel"
+# my_str = len(my_str)
+# if len(my_str) < 5:
+#     print(my_str + "Ruslan")
+# else:
+#     print(my_str)
